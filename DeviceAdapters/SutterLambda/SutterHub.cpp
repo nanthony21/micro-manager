09fdde84a85897f4cc30a4357d3dbea5fe684e95
Nick Anthony <nicholas.anthony@northwestern.edu>
2018-10-04 18:34:28 -0500

    start implementing methods

--- DeviceAdapters/SutterLambda/SutterHub.cpp
+++ DeviceAdapters/SutterLambda/SutterHub.cpp
@@ -14,3 +14,16 @@ SutterHub::~SutterHub()
 int SutterHub::DetectInstalledDevices() {
 
 }
+
+int OnPort(MM::PropertyBase* pProp, MM::ActionType eAct);
+
+bool ControllerBusy();
+int GoOnLine(unsigned long answerTimeoutMs);
+int GetControllerType(unsigned long answerTimeoutMs, std::string& type, std::string& id);
+int GetStatus(unsigned long answerTimeoutMs, unsigned char* status);
+
+int SutterHub::SetCommand(const std::vector<unsigned char> command, const std::vector<unsigned char> alternateEcho, const unsigned long answerTimeoutMs, const bool responseRequired, const bool CRExpected) {
+	std::vector<unsigned char> _;
+	return  SetCommand(command, alternateEcho, answerTimeoutMs, _, responseRequired, CRExpected);
+}
+
