02d47d0498226f5bce62a693d52596c111c614af
Nick Anthony <nicholas.anthony@northwestern.edu>
2018-10-04 18:05:00 -0500

    Remove a bunch of unneccesary args left over from static approach.

--- DeviceAdapters/SutterLambda/SutterHub.h
+++ DeviceAdapters/SutterLambda/SutterHub.h
@@ -19,14 +19,16 @@ public:
 	int OnPort(MM::PropertyBase* pProp, MM::ActionType eAct);
 
 	//From old sutterutil class
-	static bool ControllerBusy(MM::Device& device, MM::Core& core, std::string port, unsigned long answerTimeoutMs);
-	static int GoOnLine(MM::Device& device, MM::Core& core, std::string port, unsigned long answerTimeoutMs);
-	static int GetControllerType(MM::Device& device, MM::Core& core, std::string port, unsigned long answerTimeoutMs, std::string& type, std::string& id);
-	static int GetStatus(MM::Device& device, MM::Core& core, std::string port, unsigned long answerTimeoutMs, unsigned char* status);
-	static int SetCommand(MM::Device& device, MM::Core& core, const std::string port, const std::vector<unsigned char> command, const std::vector<unsigned char> alternateEcho, const unsigned long answerTimeoutMs, const bool responseRequired = true, const bool CRexpected = true);
-	static int SetCommand(MM::Device& device, MM::Core& core, const std::string port, const std::vector<unsigned char> command, const std::vector<unsigned char> alternateEcho, const unsigned long answerTimeoutMs, std::vector<unsigned char>& response, const bool responseRequired = true, const bool CRExpected = true);
+	static bool ControllerBusy();
+	static int GoOnLine(unsigned long answerTimeoutMs);
+	static int GetControllerType(unsigned long answerTimeoutMs, std::string& type, std::string& id);
+	static int GetStatus(unsigned long answerTimeoutMs, unsigned char* status);
+	static int SetCommand(const std::vector<unsigned char> command, const std::vector<unsigned char> alternateEcho, const unsigned long answerTimeoutMs, const bool responseRequired = true, const bool CRexpected = true);
+	static int SetCommand(const std::vector<unsigned char> command, const std::vector<unsigned char> alternateEcho, const unsigned long answerTimeoutMs, std::vector<unsigned char>& response, const bool responseRequired = true, const bool CRExpected = true);
 	// some commands don't send a \r!!!
-	static int SetCommandNoCR(MM::Device& device, MM::Core& core, const std::string port, const std::vector<unsigned char> command, const std::vector<unsigned char> alternateEcho, const unsigned long answerTimeoutMs, std::vector<unsigned char>& response, const bool responseRequired = true);
+	static int SetCommandNoCR(const std::vector<unsigned char> command, const std::vector<unsigned char> alternateEcho, const unsigned long answerTimeoutMs, std::vector<unsigned char>& response, const bool responseRequired = true);
+private:
+	const std::string port;
 
 };
 
