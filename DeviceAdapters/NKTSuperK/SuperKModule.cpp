2fba9cca91355d037e86cb848eda36c4cffdf604
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-10-10 15:46:39 -0500

    add licenses. Change sdk location to be in the 3rdparty folder

--- DeviceAdapters/NKTSuperK/SuperKModule.cpp
+++ DeviceAdapters/NKTSuperK/SuperKModule.cpp
@@ -1,3 +1,34 @@
+///////////////////////////////////////////////////////////////////////////////
+// FILE:          SuperKModule.cpp
+// PROJECT:       Micro-Manager
+// SUBSYSTEM:     DeviceAdapters
+//-----------------------------------------------------------------------------
+// DESCRIPTION:   SuperK Laser by NKT Photonics
+// COPYRIGHT:     2019 Backman Biophotonics Lab, Northwestern University
+//                All rights reserved.
+//
+//                This library is free software; you can redistribute it and/or
+//                modify it under the terms of the GNU Lesser General Public
+//                License as published by the Free Software Foundation.
+//
+//                This library is distributed in the hope that it will be
+//                useful, but WITHOUT ANY WARRANTY; without even the implied
+//                warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//                PURPOSE. See the GNU Lesser General Public License for more
+//                details.
+//
+//                IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+//                LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//                EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+//
+//                You should have received a copy of the GNU Lesser General
+//                Public License along with this library; if not, write to the
+//                Free Software Foundation, Inc., 51 Franklin Street, Fifth
+//                Floor, Boston, MA 02110-1301 USA.
+//
+// AUTHOR:        Nick Anthony
+
+
 #ifdef WIN32
 #include <windows.h>
 #define snprintf _snprintf 
