f4cc95788b4fde16caf7c27b784b89b68833d1d8
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-10-22 13:12:39 -0500

    remove unused error codes

--- DeviceAdapters/AAAOTF/AAAOTF.h
+++ DeviceAdapters/AAAOTF/AAAOTF.h
@@ -16,23 +16,8 @@
 //////////////////////////////////////////////////////////////////////////////
 // Error codes
 //
-#define ERR_UNKNOWN_POSITION         10002
 #define ERR_PORT_CHANGE_FORBIDDEN    10004
-#define ERR_SET_POSITION_FAILED      10005
-#define ERR_INVALID_STEP_SIZE        10006
-#define ERR_INVALID_MODE             10008
-#define ERR_UNRECOGNIZED_ANSWER      10009
-#define ERR_UNSPECIFIED_ERROR        10010
-#define ERR_COMMAND_ERROR            10201
-#define ERR_PARAMETER_ERROR          10202
-#define ERR_RECEIVE_BUFFER_OVERFLOW  10204
-#define ERR_COMMAND_OVERFLOW         10206
-#define ERR_PROCESSING_INHIBIT       10207
-#define ERR_PROCESSING_STOP_ERROR    10208
 
-#define ERR_OFFSET 10100
-#define ERR_AOTF_OFFSET 10200
-#define ERR_INTENSILIGHTSHUTTER_OFFSET 10300
 
 //These variables are defined in moduleAPI.cpp, we include them here to make them global to the device adapter.
 extern const char* g_AOTF; 
