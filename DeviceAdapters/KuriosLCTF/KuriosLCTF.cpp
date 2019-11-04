e9dcf4c7e71069d6bd01a99ff0bf9002807808e4
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-11-04 14:28:22 -0600

    specify which properties to implement.

--- DeviceAdapters/KuriosLCTF/KuriosLCTF.cpp
+++ DeviceAdapters/KuriosLCTF/KuriosLCTF.cpp
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-// FILE:          KuriosLCTF.h
+// FILE:          KuriosLCTF.cpp
 // PROJECT:       Micro-Manager
 // SUBSYSTEM:     DeviceAdapters
 //-----------------------------------------------------------------------------
@@ -28,45 +28,16 @@
 //
 // AUTHOR:        Nick Anthony
 
-#pragma once
+#include "KuriosLCTF.h"
 
-#include "../../MMDevice/MMDevice.h"
-#include "../../MMDevice/DeviceBase.h"
-#include "KURIOS_COMMAND_LIB.h"
-#include <string>
-#include <stdint.h>
+KuriosLCTF::KuriosLCTF();
+KuriosLCTF::~KuriosLCTF();
 
+//Device API
+int KuriosLCTF::Initialize();
+int KuriosLCTF::Shutdown();
+void KuriosLCTF::GetName(char* pName) const;
+bool KuriosLCTF::Busy(){return false;};
 
-
-class KuriosLCTF: public CGenericBase<KuriosLCTF> {
-public:
-	KuriosLCTF();
-	~KuriosLCTF();
-
-	//Device API
-	int Initialize();
-	int Shutdown();
-	void GetName(char* pName) const;
-	bool Busy(){return false;};
-
-	//Properties
-	id ro
-	spec ro
-	opticalheadtype ro
-	outputmode
-	bandwidthmode
-	wavelength - sequence data, insert, delete 
-	seqTimeInterval
-	seqBandwidthMode
-	status ro
-	temperature ro
-	triggerout mode
-	forcetrigger
-
-
-	int onEmission(MM::PropertyBase* pProp, MM::ActionType eAct);
-	int onPower(MM::PropertyBase* pProp, MM::ActionType eAct);
-	int onInletTemperature(MM::PropertyBase* pProp, MM::ActionType eAct);
-private:
-	std::string name_;
-};
+//Properties
+int KuriosLCTF::onEmission(MM::PropertyBase* pProp, MM::ActionType eAct);
