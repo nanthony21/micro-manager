759f6edf34d7e5c6fff9200a6e4b1ba2c8db8a12
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-11-11 13:22:02 -0600

    add tunable filter source file

---
+++ DeviceAdapters/CrystalTechnology/CTTunableFilter.cpp
@@ -0,0 +1,15 @@
+#include "CrystalTechnology.h"
+
+#define BREAKERR if (ret!=0) { return ret; }
+
+CTTunableFilter::CTTunableFilter(): CTBase() {}
+
+int CTTunableFilter::Initialize() {
+	int ret = CTBase.Initialize();
+	BREAKERR
+
+	CPropertyAction* pAct = new CPropertyAction(this, &CTTunableFilter::onWavelength);
+	this->CreateProperty(
+
+	CPropertyAction* pAct = new CPropertyAction(this, &CTTunableFilter::onBandwidth);
+}
