a32e92046c9512670ff5954a8777be9608aa15a3
Nick <nickmanthony@hotmail.com>
2020-12-21 22:47:43 -0600

    Revert "remove unused deprecated methods"
    This reverts commit 5c8fada66afaaa0feb9b1c9d1c329749f0253aac.

--- mmstudio/src/main/java/org/micromanager/internal/MMStudio.java
+++ mmstudio/src/main/java/org/micromanager/internal/MMStudio.java
@@ -629,6 +629,137 @@ public final class MMStudio implements Studio, CompatibilityInterface, Applicati
       return AcqControlDlg.getShouldHideMDADisplay();
    }
 
+   public void setCenterQuad() {
+      ImagePlus curImage = WindowManager.getCurrentImage();
+      if (curImage == null) {
+         return;
+      }
+
+      Rectangle r = curImage.getProcessor().getRoi();
+      int width = r.width / 2;
+      int height = r.height / 2;
+      int xOffset = r.x + width / 2;
+      int yOffset = r.y + height / 2;
+
+      curImage.setRoi(xOffset, yOffset, width, height);
+      Roi roi = curImage.getRoi();
+      try {
+         setROI(updateROI(roi));
+      }
+      catch (Exception e) {
+         // Core failed to set new ROI.
+         logs().logError(e, "Unable to set new ROI");
+      }
+   }
+
+   public void setROI() {
+      ImagePlus curImage = WindowManager.getCurrentImage();
+      if (curImage == null) {
+         logs().showError("There is no open image window.");
+         return;
+      }
+
+      Roi roi = curImage.getRoi();
+      if (roi == null) {
+         // Nothing to be done.
+         logs().showError("There is no selection in the image window.\nUse the ImageJ rectangle tool to draw the ROI.");
+         return;
+      }
+      if (roi.getType() == Roi.RECTANGLE) {
+         try {
+            setROI(updateROI(roi));
+         }
+         catch (Exception e) {
+            // Core failed to set new ROI.
+            logs().logError(e, "Unable to set new ROI");
+         }
+         return;
+      }
+      // Dealing with multiple ROIs; this may not be supported.
+      try {
+         if (!(roi instanceof ShapeRoi && core_.isMultiROISupported())) {
+            logs().showError("ROI must be a rectangle.Use the ImageJ rectangle tool to draw the ROI.");
+            return;
+         }
+      }
+      catch (Exception e) {
+         logs().showError("Unable to determine if multiple ROIs is supported");
+         return;
+      }
+      // Generate list of rectangles for the ROIs.
+      ArrayList<Rectangle> rois = new ArrayList<>();
+      for (Roi subRoi : ((ShapeRoi) roi).getRois()) {
+         // HACK: just use the bounding box of each sub-ROI. Determining if
+         // sub-ROIs are rectangles is difficult (they "decompose" to Polygons
+         // once there's more than one at a time, so as far as I can tell we
+         // would have to test each angle of each polygon to see if it's
+         // 90 degrees and has the correct handedness), and this provides a
+         // good- enough solution for now.
+         rois.add(updateROI(subRoi));
+      }
+      try {
+         setMultiROI(rois);
+      }
+      catch (Exception e) {
+         // Core failed to set new ROI.
+         logs().logError(e, "Unable to set new ROI");
+      }
+   }
+
+   /**
+    * Adjust the provided rectangular ROI based on any current ROI that may be
+    * in use.
+    */
+   private Rectangle updateROI(Roi roi) {
+      Rectangle r = roi.getBounds();
+
+      // If the image has ROI info attached to it, correct for the offsets.
+      // Otherwise, assume the image was taken with the current camera ROI
+      // (which is a horrendously buggy way to do things, but that was the
+      // old behavior and I'm leaving it in case there are cases where it is
+      // necessary).
+      Rectangle originalROI = null;
+
+      DataViewer viewer = displays().getActiveDataViewer();
+      if (viewer != null) {
+         try {
+            List<Image> images = viewer.getDisplayedImages();
+            // Just take the first one.
+            originalROI = images.get(0).getMetadata().getROI();
+         }
+         catch (IOException e) {
+            ReportingUtils.showError(e, "There was an error determining the selected ROI");
+         }
+      }
+
+      if (originalROI == null) {
+         try {
+            originalROI = core().getROI();
+         }
+         catch (Exception e) {
+            // Core failed to provide an ROI.
+            logs().logError(e, "Unable to get core ROI");
+            return null;
+         }
+      }
+
+      r.x += originalROI.x;
+      r.y += originalROI.y;
+      return r;
+   }
+
+   public void clearROI() {
+      live().setSuspended(true);
+      try {
+         core_.clearROI();
+         staticInfo_.refreshValues();
+
+      } catch (Exception e) {
+         ReportingUtils.showError(e);
+      }
+      live().setSuspended(false);
+   }
+
    @Override
    public CMMCore core() {
       return core_;
@@ -1621,8 +1752,51 @@ public final class MMStudio implements Studio, CompatibilityInterface, Applicati
       return alerts();
    }
 
+
    public UiMovesStageManager getUiMovesStageManager () {
       return uiMovesStageManager_;
+   }
+
+   @Override
+   @Deprecated
+   public AffineTransform getCameraTransform(String config) {
+      // Try the modern way first
+      double[] defaultParams = new double[0];
+      double[] params = profile().getSettings(MMStudio.class).
+              getDoubleList(AFFINE_TRANSFORM + config, defaultParams);
+      if (params != null && params.length == 6) {
+         return new AffineTransform(params);
+      }
+
+      // The early 2.0-beta way of storing as a serialized object.
+      PropertyMap studioSettings = profile().
+            getSettings(MMStudio.class).toPropertyMap();
+      AffineTransform result = (AffineTransform)
+         ((DefaultPropertyMap) studioSettings).getLegacySerializedObject(
+               AFFINE_TRANSFORM_LEGACY + config, null);
+      if (result != null) {
+         // Save it the new way
+         setCameraTransform(result, config);
+         return result;
+      }
+
+      // For backwards compatibility, try retrieving it from the 1.4
+      // Preferences instead.
+      AffineTransform tfm = org.micromanager.internal.utils.UnpleasantLegacyCode.
+              legacyRetrieveTransformFromPrefs("affine_transform_" + config);
+      if (tfm != null) {
+         // Save it the new way.
+         setCameraTransform(tfm, config);
+      }
+      return tfm;
+   }
+
+   @Override
+   @Deprecated
+   public void setCameraTransform(AffineTransform transform, String config) {
+      double[] params = new double[6];
+      transform.getMatrix(params);
+      profile().getSettings(MMStudio.class).putDoubleList(AFFINE_TRANSFORM + config, params);
    }
 
    public double getCachedXPosition() {
