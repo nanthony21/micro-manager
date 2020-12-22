c6bedbdda53b4a7e0f53a7914e8ca80110ae1e52
Nick <nickmanthony@hotmail.com>
2020-12-21 18:24:46 -0600

    remove lots of unused public methods.

--- mmstudio/src/main/java/org/micromanager/internal/MMStudio.java
+++ mmstudio/src/main/java/org/micromanager/internal/MMStudio.java
@@ -544,12 +544,6 @@ public final class MMStudio implements Studio, CompatibilityInterface, Applicati
       scriptPanel_.setVisible(true);
    }
 
-   private void handleError(String message) {
-      live().setLiveModeOn(false);
-      JOptionPane.showMessageDialog(frame_, message);
-      core_.logMessage(message);
-   }
-
    /**
     * Spawn a new thread to load the acquisition engine jar, because this
     * takes significant time (TODO: Does it really, not that it is
@@ -629,141 +623,6 @@ public final class MMStudio implements Studio, CompatibilityInterface, Applicati
             ReportingUtils.logError(e, "Couldn't set exposure time.");
          }
       } // End synchronization check
-   }
-
-   public boolean getHideMDADisplayOption() {
-      return AcqControlDlg.getShouldHideMDADisplay();
-   }
-
-   public void setCenterQuad() {
-      ImagePlus curImage = WindowManager.getCurrentImage();
-      if (curImage == null) {
-         return;
-      }
-
-      Rectangle r = curImage.getProcessor().getRoi();
-      int width = r.width / 2;
-      int height = r.height / 2;
-      int xOffset = r.x + width / 2;
-      int yOffset = r.y + height / 2;
-
-      curImage.setRoi(xOffset, yOffset, width, height);
-      Roi roi = curImage.getRoi();
-      try {
-         setROI(updateROI(roi));
-      }
-      catch (Exception e) {
-         // Core failed to set new ROI.
-         logs().logError(e, "Unable to set new ROI");
-      }
-   }
-
-   public void setROI() {
-      ImagePlus curImage = WindowManager.getCurrentImage();
-      if (curImage == null) {
-         logs().showError("There is no open image window.");
-         return;
-      }
-
-      Roi roi = curImage.getRoi();
-      if (roi == null) {
-         // Nothing to be done.
-         logs().showError("There is no selection in the image window.\nUse the ImageJ rectangle tool to draw the ROI.");
-         return;
-      }
-      if (roi.getType() == Roi.RECTANGLE) {
-         try {
-            setROI(updateROI(roi));
-         }
-         catch (Exception e) {
-            // Core failed to set new ROI.
-            logs().logError(e, "Unable to set new ROI");
-         }
-         return;
-      }
-      // Dealing with multiple ROIs; this may not be supported.
-      try {
-         if (!(roi instanceof ShapeRoi && core_.isMultiROISupported())) {
-            handleError("ROI must be a rectangle.\nUse the ImageJ rectangle tool to draw the ROI.");
-            return;
-         }
-      }
-      catch (Exception e) {
-         handleError("Unable to determine if multiple ROIs is supported");
-         return;
-      }
-      // Generate list of rectangles for the ROIs.
-      ArrayList<Rectangle> rois = new ArrayList<>();
-      for (Roi subRoi : ((ShapeRoi) roi).getRois()) {
-         // HACK: just use the bounding box of each sub-ROI. Determining if
-         // sub-ROIs are rectangles is difficult (they "decompose" to Polygons
-         // once there's more than one at a time, so as far as I can tell we
-         // would have to test each angle of each polygon to see if it's
-         // 90 degrees and has the correct handedness), and this provides a
-         // good- enough solution for now.
-         rois.add(updateROI(subRoi));
-      }
-      try {
-         setMultiROI(rois);
-      }
-      catch (Exception e) {
-         // Core failed to set new ROI.
-         logs().logError(e, "Unable to set new ROI");
-      }
-   }
-
-   /**
-    * Adjust the provided rectangular ROI based on any current ROI that may be
-    * in use.
-    */
-   private Rectangle updateROI(Roi roi) {
-      Rectangle r = roi.getBounds();
-
-      // If the image has ROI info attached to it, correct for the offsets.
-      // Otherwise, assume the image was taken with the current camera ROI
-      // (which is a horrendously buggy way to do things, but that was the
-      // old behavior and I'm leaving it in case there are cases where it is
-      // necessary).
-      Rectangle originalROI = null;
-
-      DataViewer viewer = displays().getActiveDataViewer();
-      if (viewer != null) {
-         try {
-            List<Image> images = viewer.getDisplayedImages();
-            // Just take the first one.
-            originalROI = images.get(0).getMetadata().getROI();
-         }
-         catch (IOException e) {
-            ReportingUtils.showError(e, "There was an error determining the selected ROI");
-         }
-      }
-
-      if (originalROI == null) {
-         try {
-            originalROI = core().getROI();
-         }
-         catch (Exception e) {
-            // Core failed to provide an ROI.
-            logs().logError(e, "Unable to get core ROI");
-            return null;
-         }
-      }
-
-      r.x += originalROI.x;
-      r.y += originalROI.y;
-      return r;
-   }
-
-   public void clearROI() {
-      live().setSuspended(true);
-      try {
-         core_.clearROI();
-         staticInfo_.refreshValues();
-
-      } catch (Exception e) {
-         ReportingUtils.showError(e);
-      }
-      live().setSuspended(false);
    }
 
    @Override
@@ -1600,17 +1459,6 @@ public final class MMStudio implements Studio, CompatibilityInterface, Applicati
       core_.setROI(r.x, r.y, r.width, r.height);
       staticInfo_.refreshValues();
       live().setSuspended(false);
-   }
-
-   public void setMultiROI(List<Rectangle> rois) throws Exception {
-      live().setSuspended(true);
-      core_.setMultiROI(rois);
-      staticInfo_.refreshValues();
-      live().setSuspended(false);
-   }
-
-   public void setAcquisitionEngine(AcquisitionWrapperEngine eng) {
-      acqEngine_ = eng;
    }
 
    @Override
